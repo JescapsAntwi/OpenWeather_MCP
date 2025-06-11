8d5582da1b21de75a0631e72fa93dd0ec7057cb6
JescapsAntwi <antwijescaps1@gmail.com>
2025-06-11 20:13:09 +0000

    Modified call-current.js file

--- tools/openweathermap/open-weather-map/call-current-weather-data-for-one-location.js
+++ tools/openweathermap/open-weather-map/call-current-weather-data-for-one-location.js
@@ -1,5 +1,5 @@
 /**
- * Function to get current weather data for a specified locat.
+ * Function to get current weather data for a specified loca.
  *
  * @param {Object} args - Arguments for the weather data request.
  * @param {string} [args.q] - City name for the weather query (e.g., "London").
